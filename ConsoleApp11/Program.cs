5039775ff9fd887ec30d51eebb8fed1660bc9f14
Ira <mazuririnochkacat@gmail.com>
2023-05-18 20:33:09 +0300

    all

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -57,9 +57,60 @@ namespace ConsoleApp11
                 Console.WriteLine(element);
             }
 
+            // task4
+            Random random = new Random();
+            int[] numbers = new int[10000];
+
+            for (int i = 0; i < 10000; i++)
+            {
+                numbers[i] = random.Next(1, 10001);
+            }
+
+            string evenNumbersFile = "D:\\visualST\\ConsoleApp11\\ConsoleApp11\\even_numbers.txt";
+            string oddNumbersFile = "D:\\visualST\\ConsoleApp11\\ConsoleApp11\\odd_numbers.txt";
+
+            using (StreamWriter evenWriter = new StreamWriter(evenNumbersFile))
+            using (StreamWriter oddWriter = new StreamWriter(oddNumbersFile))
+            {
+                foreach (int number in numbers)
+                {
+                    if (number % 2 == 0)
+                    {
+                        evenWriter.WriteLine(number);
+                    }
+                    else
+                    {
+                        oddWriter.WriteLine(number);
+                    }
+                }
+            }
+
+            int evenCount = CountNumbers(evenNumbersFile);
+            int oddCount = CountNumbers(oddNumbersFile);
+
+            Console.WriteLine("Статистика:");
+            Console.WriteLine($"Загальна кількість чисел: {numbers.Length}");
+            Console.WriteLine($"Кількість парних чисел: {evenCount}");
+            Console.WriteLine($"Кількість непарних чисел: {oddCount}");
+
+            Console.ReadLine();
+        }
+
+        static int CountNumbers(string fileName)
+        {
+            int count = 0;
+
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                while (reader.ReadLine() != null)
+                {
+                    count++;
+                }
+            }
+
+            return count;
 
 
-          
 
         }
     }
